e0f5f74b09405ca4c0d95e3972ab8627ef435524
jaenny <hongryeon_21@naver.com>
2022-09-16 23:34:23 +0900

    로그인 페이지 UI 생성하기

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import React from "react";
+import InputGroup from "../components/InputGroup";
+
+const Login = () => {
+	const handleSubmit = () => {};
+	return (
+		<div className="bg-white">
+			<div className="flex flex-col items-center justify-content h-screen p-6">
+				<div className="w-10/12 mx-auto md:w-96">
+					<h1 className="mb-2 text-lg font-medium">회원가입</h1>
+					<form onSubmit={handleSubmit}>
+						<InputGroup
+							placeholder="Username"
+							value={username}
+							setValue={setUsername}
+							error={errors.username}
+						/>
+						<InputGroup
+							placeholder="Password"
+							value={password}
+							setValue={setPassword}
+							error={errors.password}
+						/>
+						<button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+							로그인
+						</button>
+					</form>
+					<small>
+						아직 회원이 아닌가요?
+						<Link href="/register">
+							<a className="ml-1 text-blue-500">회원가입</a>
+						</Link>
+					</small>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;

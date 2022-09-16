8c780a897a2271cb1ff4d93a180401890f742200
jaenny <hongryeon_21@naver.com>
2022-09-16 22:54:21 +0900

    회원 가입 페이지 기능 생성(2)

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from "express";
+
+const register = async(req:Request, res:Response) => {
+  const {email, username, password} = req.body;
+}
+
+const router = Router();
+router.post('/register',register);
+
+export default router;
---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
+import authRoutes from './routes/auth'
+import cors from 'cors'
+
+const app = express();
+const origin = "http://localhost:3000"
+
+app.use(cors({
+  origin
+}))
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get('/', (_,res)=>res.send("running"));
+app.use("/api/auth", authRoutes)
+
+let port = 4000;
+
+app.listen(port, async()=>{
+  console.log(`Server running at http://localhost:${port}`)
+  AppDataSource.initialize().then(async () => {
+    console.log("Database initialized...")
+}).catch(error => console.log(error))
+})

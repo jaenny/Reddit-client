ec6c38f7d9755729b73928d7fc5101ddfac7806f
jaenny <hongryeon_21@naver.com>
2022-09-17 14:31:27 +0900

    로그인 시 cookie 처리

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -28,7 +28,7 @@ const Login = () => {
 		<div className="bg-white">
 			<div className="flex flex-col items-center justify-content h-screen p-6">
 				<div className="w-10/12 mx-auto md:w-96">
-					<h1 className="mb-2 text-lg font-medium">회원가입</h1>
+					<h1 className="mb-2 text-lg font-medium">로그인</h1>
 					<form onSubmit={handleSubmit}>
 						<InputGroup
 							placeholder="Username"
